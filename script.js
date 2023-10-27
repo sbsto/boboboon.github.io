c0db37ce49b2ce9ee95ffcbabfed52c5773cfd1d
Lucas Curtin <lucas.curtin@gmail.com>
2023-10-27 17:08:48 +0100

    Nasa API attempt

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 // script.js
 
-// Function to add animated text elements
-function addAnimatedText() {
-    var main = document.querySelector("main");
+// Function to fetch and display the APOD
+async function fetchAndDisplayAPOD() {
+    const apiKey = await fetch('file:///C:/Users/Intel%20NUC/Desktop/nasa_api_key.txt')
+        .then(response => response.text())
+        .catch(error => {
+            console.error('Error fetching API key:', error);
+            return '';
+        });
 
-    // Create a div for the animated text
-    var animationDiv = document.createElement("div");
-    animationDiv.classList.add("continuous-animation");
-    animationDiv.textContent = "Welcome to Your Website!"; // Change this text as needed
+    const apodContainer = document.getElementById('apod-container');
+    const apodImage = document.getElementById('apod-image');
+    const apodDescription = document.getElementById('apod-description');
 
-    // Append the animated text to the main content
-    main.appendChild(animationDiv);
+    if (apiKey) {
+        const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.url && data.explanation) {
+                    apodImage.src = data.url;
+                    apodDescription.textContent = data.explanation;
+                } else {
+                    console.error('Invalid response from APOD API:', data);
+                    apodContainer.innerHTML = '<p>Unable to fetch APOD.</p>';
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching APOD:', error);
+                apodContainer.innerHTML = '<p>Unable to fetch APOD.</p>';
+            });
+    } else {
+        console.error('API key is missing or invalid.');
+        apodContainer.innerHTML = '<p>Unable to fetch APOD.</p>';
+    }
 }
 
-// Add the animated text when the page is loaded
-window.addEventListener("load", addAnimatedText);
+// Add the event listener to fetch and display the APOD when the page is loaded
+window.addEventListener('load', fetchAndDisplayAPOD);
